907ac124b90c2827b7a220c376182e766de5a1a8
ravkasu <ravkasu@gmail.com>
2024-10-15 00:00:59 +0530

    send email php file and gitignore changes

---
+++ src/app/config/send-email.php
@@ -0,0 +1,26 @@
+<?php
+//get data from form  
+
+$firstname = $_POST['first-name'];
+$lastname = $_POST['last-name'];
+$email= $_POST['email'];
+$phonenumber= $_POST['phone-number'];
+$company= $_POST['company'];
+$companywebsite= $_POST['companywebsite'];
+$message= $_POST['message'];
+$to = "contact@rapdservice.co.uk";
+$subject = "New Lead Alert - Mail From RAPD Website";
+$txt ="First Name = ". $firstname . "\r\n  
+    Last Name = ". $lastname . "\r\n  
+    Email = " . $email . "\r\n  
+    Phone Number = ". $phonenumber . "\r\n 
+    Company = ". $company . "\r\n   
+    Company Website = ". $companywebsite . "\r\n  
+    Message =" . $message;
+$headers = "From: noreply@rapdservice.co.uk" . "\r\n" . "CC: contact@rapdservice.co.uk";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:sent.html");
+?>
